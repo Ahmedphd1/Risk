b4a46957d6a28573b531a862ebb16ed296c996b7
ahmed <ahma3660@elev.tec.dk>
2024-03-23 16:52:09 +0100

    cleaned project and updated riskmap

--- Assets/Resources/scripts/enums/playertags.cs
+++ Assets/Resources/scripts/enums/playertags.cs
@@ -1,10 +1,5 @@
 public static class playertags
 {
-    public static string player1 { get; } = "player1";
-    public static string player2 { get; } = "player2";
-    public static string neutral { get; } = "neutral";
-    public static string anyplayer { get; } = "player";
-
     public static string selectcircle { get; } = "selectcircle";
 
     public static string healthbarsystem { get; } = "healthbarsystem";
--- Assets/Resources/scripts/setupmap.cs
+++ Assets/Resources/scripts/setupmap.cs
@@ -7,7 +7,6 @@ using Unity.Netcode;
 
 public class setupmap : MonoBehaviour
 {
-    private GameObject map;
     [SerializeField] private Button server;
     [SerializeField] private Button host;
     [SerializeField] private Button client;
@@ -20,13 +19,11 @@ public class setupmap : MonoBehaviour
         server.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartServer();
-            startmap();
         });
 
         host.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartHost();
-            startmap();
         });
 
         client.onClick.AddListener(() =>
@@ -35,56 +32,7 @@ public class setupmap : MonoBehaviour
         });
     }
 
-    private void startmap()
-    {
-        map = this.transform.gameObject;
-        map.layer = LayerMask.NameToLayer(layers.floor);
-        setupbasesurface(map);
-        setupnavmesh(map);
-    }
-
-    bool setupbasesurface(GameObject map)
-    {
-        if (map != null)
-        {
-            map.transform.GetChild(0).transform.GetChild(0).gameObject.tag = playertags.player1;
-            map.transform.GetChild(1).transform.GetChild(0).gameObject.tag = playertags.player2;
-            map.transform.GetChild(2).transform.GetChild(0).gameObject.tag = playertags.neutral;
-            // setting up box collider for detection of minions
-            for (int i = 0; i <= map.transform.childCount - 1; i++)
-            {
-                GameObject basesurface = map.transform.GetChild(i).gameObject;
-
-                if (basesurface != null)
-                {
-                    basesurface.layer = LayerMask.NameToLayer(layers.floor);
-                    basesurface.AddComponent<MeshCollider>();
-                    GameObject baseobject = basesurface.transform.GetChild(0).gameObject;
-                    setupbasebuilding(baseobject, basesurface.GetComponent<MeshCollider>(), basesurface.GetComponent<Renderer>());
-                }
-                else
-                {
-                }
-            }
-        } else
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-    bool setupnavmesh(GameObject map)
-    {
-        // adding navigation components
-        map.AddComponent<MeshCollider>();
-        NavMeshSurface navmesh = map.AddComponent<NavMeshSurface>();
-        navmesh.layerMask = LayerMask.GetMask(layers.floor);
-        navmesh.BuildNavMesh();
-        return true;
-    }
-
-    bool setupbasebuilding(GameObject basebuilding, MeshCollider surfacemesh, Renderer surfacerenderer)
+    void setupbasebuilding(GameObject basebuilding, MeshCollider surfacemesh, Renderer surfacerenderer)
     {
         // adding basecontroller to basebuilding
 
@@ -98,13 +46,5 @@ public class setupmap : MonoBehaviour
         // adding spawnminion to basebuilding
         spawnminion spawnminionscript = basebuilding.transform.AddComponent<spawnminion>();
         spawnminionscript.spawntime = 5;
-
-        // setup navmesh obstacle
-        NavMeshObstacle navmeshobstacle = basebuilding.AddComponent<NavMeshObstacle>();
-        navmeshobstacle.shape = NavMeshObstacleShape.Capsule;
-        navmeshobstacle.radius *= 1.5f;
-        return true;
     }
-
-
 }
